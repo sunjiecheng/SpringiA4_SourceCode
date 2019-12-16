afc72fedaabfc909678ca87f625c459f6c359a0d
jiecheng <tojiecheng@163.com>
2019-12-16 17:31:17 +0800

    提交第五章

--- spring-in-action/src/main/java/chapter05/spittr/web/SpittleController.java
+++ spring-in-action/src/main/java/chapter05/spittr/web/SpittleController.java
@@ -5,10 +5,12 @@ import chapter05.spittr.data.SpittleRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -32,5 +34,18 @@ public class SpittleController {
     public List<Spittle> spittles(@RequestParam(value = "max", defaultValue = MAX_LONG_AS_STRING) long max,
                                   @RequestParam(value = "count", defaultValue = "20") int count) {
         return spittleRepository.findSpittles(max, count);
+    }
+
+    @RequestMapping(value = "/{spittleId}", method = RequestMethod.GET)
+    public String spittle(@PathVariable long spittleId, Model model) {
+        model.addAttribute(spittleRepository.findOne(spittleId));
+        return "spittle";
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public String saveSpittle(SpittleForm form, Model model) throws Exception {
+        spittleRepository.save(
+                new Spittle(null, form.getMessage(), new Date(), form.getLongitude(), form.getLatitude()));
+        return "redirect:/spittles";
     }
 }
